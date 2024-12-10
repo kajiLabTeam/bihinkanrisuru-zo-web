abfd1e4e1b7ab232e562f9339ab5c188a0191152
satooru65536 <ryuto4619@gmail.com>
2024-12-10 20:04:41 +0900

    feat: プロジェクトのセットアップ

---
+++ src/pages/top.tsx
@@ -0,0 +1,9 @@
+import { Button, Heading } from "@radix-ui/themes";
+
+export default function TopPage() {
+  return (
+    <Heading as="h1">
+      <Button>サンプル</Button>
+    </Heading>
+  );
+}
---
+++ src/pages/user-list.tsx
@@ -0,0 +1,5 @@
+import { Heading } from "@radix-ui/themes";
+
+export default function UserListPage() {
+  return <Heading as="h1">ユーザ一覧</Heading>;
+}
