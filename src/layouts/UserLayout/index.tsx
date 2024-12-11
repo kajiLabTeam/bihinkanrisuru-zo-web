eea82771686cd5c6bb49a5f0e8abe4522da77ca1
karasuneo <yusei4520030401@gmail.com>
2024-12-11 12:16:04 +0900

    add: 管理者用備品登録の追加

---
+++ src/layouts/UserLayout/index.tsx
@@ -0,0 +1,21 @@
+import Header, { Navigation } from "@/components/Header";
+import { Box } from "@radix-ui/themes";
+import { Outlet } from "react-router";
+
+const navigations = [
+  {
+    name: "備品一覧",
+    path: "/user/equipments",
+  },
+] satisfies Navigation[];
+
+export default function UserLayout() {
+  return (
+    <>
+      <Header navigations={navigations} />
+      <Box pt="9">
+        <Outlet />
+      </Box>
+    </>
+  );
+}
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,14 +2,30 @@ import { Routes as ReactRouterRoutes, Route } from "react-router";
 import TopPage from "./pages/top";
 import AdminLayout from "./layouts/AdminLayout";
 import UserListPage from "./pages/admin/user-list";
+import UserEquipmentListPage from "./pages/user/equipment-list";
+import UserLayout from "./layouts/UserLayout";
+import AdminEquipmentListPage from "./pages/admin/equipment-list";
+import EquipmentRegistrationPage from "./pages/admin/equipment-registration";
+import EquipmentEditingPage from "./pages/admin/equipment-editing";
 
 export default function Routes() {
   return (
     <ReactRouterRoutes>
       <Route path="/" element={<TopPage />} />
-
       <Route path="admin" element={<AdminLayout />}>
         <Route path="users" element={<UserListPage />} />
+        <Route path="equipments" element={<AdminEquipmentListPage />} />
+        <Route
+          path="equipments/register"
+          element={<EquipmentRegistrationPage />}
+        />
+        <Route
+          path="equipments/:equipment_id/edit"
+          element={<EquipmentEditingPage />}
+        />
+      </Route>
+      <Route path="user" element={<UserLayout />}>
+        <Route path="equipments" element={<UserEquipmentListPage />} />
       </Route>
     </ReactRouterRoutes>
   );
