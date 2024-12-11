6dfa722ec2e6e6afff43d3ce5a6374179725fbc1
satooru65536 <ryuto4619@gmail.com>
2024-12-11 13:27:11 +0900

    style: format

--- src/pages/admin/equipment-editing.tsx
+++ src/pages/admin/equipment-editing.tsx
@@ -1,4 +1,4 @@
-import { Heading } from "@radix-ui/themes";
+import { Heading } from '@radix-ui/themes';
 
 export default function EquipmentEditingPage() {
   return <Heading as="h1">管理者用備品編集</Heading>;
--- src/pages/user/equipment-list.tsx
+++ src/pages/user/equipment-list.tsx
@@ -1,4 +1,4 @@
-import { Heading } from "@radix-ui/themes";
+import { Heading } from '@radix-ui/themes';
 
 export default function UserEquipmentListPage() {
   return <Heading as="h1">利用者用備品一覧</Heading>;
