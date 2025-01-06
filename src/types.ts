b36cb4e0119bf45ad5510e3c9c91269fb7ae404b
X22043 <x22043xx@aitech.ac.jp>
2025-01-06 16:11:34 +0900

    fix: 定義した型を適切に変更

--- src/types.ts
+++ src/types.ts
@@ -1,14 +1,31 @@
 type Request = {
-    ait_id?: string; //1234-0000
+    equipment_id?: string; //1234-0000
     name?: string; //備品登録名
     date?: {
-        purchase_date: string; // 購入日
+        purchase_date: Date; // 購入日
     };
     place?: string; //保管場所
     tag?: string[]; //属性
 };
 
 type Response = {
-    result: string;
+    result: "ok" | "error";
     error: string; //エラーメッセージ
+};
+
+type Equipments = {
+    equipment_id: string;
+    asset_id: string;
+    name: string;
+    status: {
+        state: string;
+        borrower?: string;
+        borrowed_date?: Date;
+    };
+    date: {
+        registration_date: Date;
+        purchase_date: Date;
+    };
+    place: string;
+    tag?: string[];
 };
