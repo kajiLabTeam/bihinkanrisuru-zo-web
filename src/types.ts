2fff68412202116012e1af9b846901b07789a736
X22043 <x22043xx@aitech.ac.jp>
2025-01-14 18:39:23 +0900

    fix: typesの括っていたところの分解

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,7 @@
 export type EquipmentsRequest = {
     equipment_id?: string; //1234-0000
     name?: string; //備品登録名
-    date?: {
-        purchase_date: Date; // 購入日
-    };
+    purchase_date: Date; // 購入日
     place?: string; //保管場所
     tag?: string[]; //属性
 };
@@ -17,15 +15,11 @@ export type Equipments = {
     equipment_id: string;
     asset_id: string;
     name: string;
-    status: {
-        state: string;
-        borrower?: string;
-        borrowed_date?: Date;
-    };
-    date: {
-        registration_date: Date;
-        purchase_date: Date;
-    };
+    state: string;
+    borrower?: string;
+    borrowed_date?: Date;
+    registration_date: Date;
+    purchase_date: Date;
     place: string;
     tag?: string[];
 };
