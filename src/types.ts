22e6a0b41ed0cea4d7b87364540663b056518d5a
SatohAyaka <sayaka310ss@docomo.ne.jp>
2025-02-25 18:37:09 +0900

    fix: 型をundefined許容に変更

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,7 @@
 export interface EquipmentsRequest {
   asset_id?: string; // 1234-0000,手動入力
   name?: string; // 備品登録名
-  purchase_date: number; // 購入日
+  purchase_date?: number; // 購入日
   place?: string; // 保管場所
   tag?: string[]; // 属性
 }
@@ -19,7 +19,7 @@ export interface Equipments {
   borrower?: string;
   borrowed_date?: number;
   registration_date: number;
-  purchase_date: number;
-  place: string;
+  purchase_date?: number;
+  place?: string;
   tag?: string[];
 }
