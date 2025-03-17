db031b0b952b5f97edd242d0b4be39738b376dd7
karasuneo <yusei4520030401@gmail.com>
2025-03-17 13:58:15 +0900

    feat: ルーティング構造を整理し、管理者レイアウト内にユーザーと機器のルートを移動

--- src/routes.tsx
+++ src/routes.tsx
@@ -14,15 +14,16 @@ export default function Routes() {
   return (
     <ReactRouterRoutes>
       <Route element={<TopPage />} path="/" />
-      <Route element={<AdminLayout />} path="admin" />
-      <Route path="users">
-        <Route element={<UserListPage />} index />
-        <Route element={<UserEditPage />} path=":user_id/edit" />
-      </Route>
-      <Route path="equipments">
-        <Route element={<AdminEquipmentListPage />} index />
-        <Route element={<EquipmentRegistrationPage />} path="register" />
-        <Route element={<EquipmentEditingPage />} path=":equipment_id/edit" />
+      <Route element={<AdminLayout />} path="admin">
+        <Route path="equipments">
+          <Route element={<AdminEquipmentListPage />} index />
+          <Route element={<EquipmentRegistrationPage />} path="register" />
+          <Route element={<EquipmentEditingPage />} path=":equipment_id/edit" />
+        </Route>
+        <Route path="users">
+          <Route element={<UserListPage />} index />
+          <Route element={<UserEditPage />} path=":user_id/edit" />
+        </Route>
       </Route>
       <Route element={<UserLayout />} path="user">
         <Route element={<UserEquipmentListPage />} path="equipments" />
